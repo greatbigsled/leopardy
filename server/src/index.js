2cb0f5bdde589440b2f9725e3e2e2bef4a0208aa
Alexander Ivanov <vahabond@ya.ru>
2020-05-16 02:44:51 +0300

    Replace front to simpler version (no TS)

--- server/src/index.js
+++ server/src/index.js
@@ -1,16 +1,84 @@
-const { Server } = require('@logux/server')
+const {Server} = require('http');
 
-const server = new Server(
-  Server.loadOptions(process, {
-    subprotocol: '1.0.0',
-    supports: '1.x',
-    root: __dirname
-  })
-)
+const Koa = require('koa');
+const serve = require('koa-static');
 
-server.auth((userId, token) => {
-  // Allow only local users until we will have a proper authentication
-  return process.env.NODE_ENV === 'development'
-})
+const bodyParser = require('koa-bodyparser');
+const router = require('./routes');
 
-server.listen()
+
+const app = new Koa()
+const server = new Server(app.callback());
+
+const io = require('socket.io')(server);
+
+app.use( bodyParser() );
+app.use( router.middleware() );
+app.use( serve('../../front/build') );
+
+
+// app.listen(3001, () => {
+//   console.log('Server started');
+// })
+
+
+
+
+server.listen(3000, (err) => {
+  if (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+  console.log('Server started');
+});
+
+
+
+io.on('connection', socket => {
+  socket.on('start', (...a) => {
+    console.log('start');
+  });
+
+  socket.on('message', ({from, message}) => {
+    socket.broadcast.emit('message', {from, message})
+  });
+});
+
+
+
+
+
+
+
+
+// const {Server} = require('http');
+
+// const Koa = require('koa');
+// const serve = require('koa-static');
+
+// const app = new Koa();
+// const server = new Server(app.callback());
+// const io = require('socket.io')(server);
+
+// app.use(serve('./public'));
+
+// server.listen(3000, (err) => {
+//   if (err) {
+//     console.error(err.message);
+//     process.exit(1);
+//   }
+//   console.log('Server started');
+// });
+
+// io.on('connection', socket => {
+
+//   socket.on('start', (...a) => {
+//     console.log('start');
+//   });
+
+//   socket.on('message', ({from, message}) => {
+//     socket.broadcast.emit('message', {from, message})
+//   });
+
+
+// });
